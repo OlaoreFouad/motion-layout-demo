192c68272e1ee5064c45a451b9dd536c64d7aeac
Foodie <olaorefouad565@gmail.com>
2021-01-29 11:33:51 +0400

    reviewed the first part of the motion layout article series, did the exercise for a view moving from left to the right again

--- app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scene_02)
+        setContentView(R.layout.activity_scene_03)
     }
 }
