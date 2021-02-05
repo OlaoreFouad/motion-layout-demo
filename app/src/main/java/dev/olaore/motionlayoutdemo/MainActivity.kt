6f42ffb54113fd47b2586e9324331b2a6d70bfaf
Foodie <olaorefouad565@gmail.com>
2021-02-06 01:37:39 +0400

    tried an animation that would probably go on groomr, a card showing a view when swiped from the left

--- app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_part_2_c)
+        setContentView(R.layout.activity_pull_left)
     }
 }
