64443c7ef9f5e326d808a3a043c750750741aab4
Foodie <olaorefouad565@gmail.com>
2021-02-25 21:15:30 +0400

    worked on an animation with an imageView that shrinks when the title is dragged up

--- app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pull_left)
+        setContentView(R.layout.activity_scene_04)
     }
 }
