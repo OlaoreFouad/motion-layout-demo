fa8fdc15191f3aef3bb6666bc3af6a7d6f8bdc5e
Olaore Fouad <olaorefouad565@gmail.com>
2021-03-05 21:35:35 +0400

    prepared two other animations for display on LinkedIn

--- app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/dev/olaore/motionlayoutdemo/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scene_04)
+        setContentView(R.layout.activity_twitter_scale)
     }
 }
